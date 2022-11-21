1cef3391dffa58f540d1e4f81459a8c7af983dfa
Mr Akhtar <mogulking@gmail.com>
2022-11-21 20:01:42 +0000

    more on components sections done

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle = 'Acme Products Manager';
 }
---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  private _listFilter: string = 'cart';
+  filteredProducts: IProduct[] = [];
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  products: IProduct[] = [
+    {
+      productId: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2021',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    },
+    {
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2021',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png',
+    },
+  ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this.listFilter = 'cart';
+  }
+
+  performFilter(value: string): IProduct[] {
+    return this.products.filter((product) => {
+      value = value.toLocaleLowerCase();
+      let productName = product.productName.toLocaleLowerCase();
+      return productName.includes(value);
+    });
+  }
+}
